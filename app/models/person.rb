e437688c69b89a6e40e4a164a0bd7b207b283fca
Kenneth To <kennethkto@gmail.com>
2015-02-26 11:49:18 -0700

    ran join table migration called assignments and create associations in models

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,8 +1,10 @@
 class Person < ActiveRecord::Base
 
-validates :last_name, presence: true
+  has_many :assignments
+  has_many :people, through: :assignments
+  validates :last_name, presence: true
 
-validates :first_name, presence: true, unless: ->(person){person.title.present?}
-validates :title, presence: true, unless: ->(person){person.first_name.present?}
+  validates :first_name, presence: true, unless: ->(person){person.title.present?}
+  validates :title, presence: true, unless: ->(person){person.first_name.present?}
 
 end
